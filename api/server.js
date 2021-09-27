8661afd93918c6848e673fb27cecd480d49bf8be
nhamilton1 <nickh3592@gmail.com>
2021-09-27 15:49:06 -0400

    finished delete crud

--- api/server.js
+++ api/server.js
@@ -55,4 +55,23 @@ server.get('/api/users/:id', async (req, res) => {
     }
 })
 
+
+server.delete('/api/users/:id', async(req, res) => {
+    try {
+        const deletedUser = await Users.remove(req.params.id)
+        if(!deletedUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            res.json(deletedUser)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The user could not be removed"
+            })
+    }
+})
+
+
 module.exports = server;
